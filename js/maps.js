ae1cdc2dafdbdb2ad2e5f9a3fb5161afa4fa14d1
Dipen Patel <dipen.patel@devstree.in>
2025-06-24 15:41:55 +0530

    html site - first commit

---
+++ js/maps.js
@@ -0,0 +1,218 @@
+
+google.maps.event.addDomListener(window, 'load', initGMap);
+
+function initGMap() {
+    var mapOptions = {
+        zoom: 11,
+        scrollwheel: false,
+        mapTypeControl: false,
+        zoomControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        disableDefaultUI: true,
+        center: new google.maps.LatLng(12.972140,77.643200),
+        styles: [
+            {
+                "featureType": "all",
+                "elementType": "labels.text.fill",
+                "stylers": [
+                    {
+                        "saturation": 36
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 60
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.text.stroke",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    },
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels.icon",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    },
+                    {
+                        "weight": 1.2
+                    }
+                ]
+            },
+            {
+                "featureType": "landscape",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 20
+                    }
+                ]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 23
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.fill",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 17
+                    }
+                ]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry.stroke",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 29
+                    },
+                    {
+                        "weight": 0.2
+                    }
+                ]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 18
+                    }
+                ]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 16
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 19
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [
+                    {
+                        "color": "#000000"
+                    },
+                    {
+                        "lightness": 10
+                    }
+                ]
+            }
+        ]
+    };
+    var mapElement = document.getElementById('map');
+    var map = new google.maps.Map(mapElement, mapOptions);
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(12.972140,77.643200),
+        map: map,
+        title: 'Jewelbox App',
+        icon: ''
+    });
+
+    // InfoWindow content
+    var contentString = '<div id="iw-container">' +
+        '<div class="iw-content">' +
+        '<h4>Jewelbox Office</h4><h5>Opening Times:</h5>' +
+        '<h6>Monday - Friday &nbsp; &nbsp; 10am - 11pm</h6>' +
+        '<h5>Address:</h5>' +
+        '<h6>2435, 4th Cross, Indiranagar, HAL 3rd Stage, Bangalore 560017 Karnataka<br>'+
+        '<br>Phone.  +91 98451 23858<br>e-mail: feedback@jewelbox.app</h6>'+
+        '</div>'+
+        '</div>';
+
+
+
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 350
+    });
+
+
+
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
+
+}
